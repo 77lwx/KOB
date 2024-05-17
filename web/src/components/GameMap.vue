5fcebd4ed8c761b518be3ef579d96889e50c660d
浪 味仙 <2909314910@qq.com>
2024-05-17 22:13:00 +0800

    一次大的更新

--- web/src/components/GameMap.vue
+++ web/src/components/GameMap.vue
@@ -1,35 +1,42 @@
 <template>
   <div ref="parent" class="gamemap">
+
+        <div class="text-box">  
+            <h2 class="box-title">玩家：{{ $store.state.user.username  }}</h2>  
+            <h2 class="box-title" v-if="$store.state.user.id == $store.state.pk.a_id">颜色:蓝色</h2>  
+            <h2 class="box-title" v-if="$store.state.user.id == $store.state.pk.b_id">颜色:红色</h2> 
+        </div>  
+
         <canvas ref="canvas" tabindex="0"></canvas>
   </div>
 </template>
 
 <script>
-import { GameMap } from "@/assets/scripts/GameMap";
-import { ref, onMounted } from 'vue'
+import{ GameMap } from "../assets/scripts/GameMap";
+import {ref , onMounted} from 'vue'
 import { useStore } from "vuex";
+export default{
+    setup(){
+        let parent=ref(null);
+        let canvas=ref(null);
+        const store=useStore();
 
-export default {
-    setup() {
-        const store = useStore();
-        let parent = ref(null);
-        let canvas = ref(null);
-
-         onMounted(() => {
+        onMounted(()=>{
             store.commit(
                 "updateGameObject",
-                new GameMap(canvas.value.getContext('2d'), parent.value, store)
+                new GameMap(canvas.value.getContext('2d'),parent.value,store),
             );
+
         });
+
 
 
         return {
             parent,
-            canvas
+            canvas,
         }
     }
 }
-
 
 </script>
 
@@ -42,5 +49,26 @@ div.gamemap{
     align-items: center;
 
 }
+
+.text-box {  
+    padding: 5px; /* 内边距 */  
+    margin-bottom: 20px; /* 下边距，用于分隔两个框 */  
+    position: relative;
+    top:20px;
+    right: 60px;
+}  
+
+.box-title {  
+    margin: 0 0 10px 0; /* 上下边距，与内容分开 */  
+    font-size: 40px; /* 字体大小 */  
+    font-weight: bold; /* 加粗 */  
+    text-shadow: 2px 2px 4px rgba(14, 116, 60, 0.5); /* 向右下偏移2px，模糊半径为4px的黑色阴影，透明度为0.5 */  
+    text-decoration: none; /* 移除下划线（默认是none） */
+}
+
+
+  
+
+
 </style>
 
